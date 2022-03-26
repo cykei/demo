08f9059e31d37ad777051b03ee40533023b34263
cykei <kelle111@naver.com>
2022-03-26 10:34:21 +0900

    Google oAuth 기능 개발중

---
+++ src/main/java/com/example/demo/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.example.demo.domain.user;
+
+import com.example.demo.domain.post.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)  //enum값이 디비에 String으로 들어간다. 설정 없으면 그냥 int로 들어감.
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(Long id, String name, String email, String picture, Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}
